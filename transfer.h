512d5869f8a59fa5f405fe2e28493fe36dc136df
zXgr <89551728+Zouhairgurtaoui@users.noreply.github.com>
2023-09-29 19:00:09 +0100

    Add files via upload

---
+++ transfer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ws2tcpip.h>
+#include <errno.h>
+
+#define BACKLOG 5
+#define SERVERPORT "3490"
+#define MAX_LINE 4096
+
